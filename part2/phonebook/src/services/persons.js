5af8a0821c87dc3071deb4315941a437c715e749
aungkhantmaw <185657819+agkmw@users.noreply.github.com>
2024-11-22 08:23:23 +0630

    add basic styling and deployment configurations

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const data = (request) => {
   return request.then((response) => response.data);
